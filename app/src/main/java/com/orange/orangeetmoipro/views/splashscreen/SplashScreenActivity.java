1616d0a90d7c9dc26b5cacbcbca8f7da870fe884
Hassine khelil <hassine.khelil@esprit.tn>
2020-06-04 11:11:43 +0100

    initial commit

---
+++ app/src/main/java/com/orange/orangeetmoipro/views/splashscreen/SplashScreenActivity.java
@@ -0,0 +1,32 @@
+package com.orange.orangeetmoipro.views.splashscreen;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.orange.orangeetmoipro.views.main.MainActivity;
+
+
+public class SplashScreenActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                goToNextActivity();
+            }
+        }, 3000);
+    }
+
+    private void goToNextActivity() {
+        Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+}
